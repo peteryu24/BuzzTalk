fe7a6540140c04dacc07b219cdc3c806850c658c
donghyeokkim <kdh301397@gmail.com>
2024-08-13 21:54:24 +0900

    [feature] Home화면 UI완성 및 뷰모델 작성

--- lib/src/view/home/room_list/room_list_view.dart
+++ lib/src/view/home/room_list/room_list_view.dart
@@ -1,23 +1,23 @@
 import 'package:alarm_app/src/model/room_model.dart';
 import 'package:alarm_app/src/service/my_room_service.dart';
+import 'package:alarm_app/src/view/base_view.dart';
 import 'package:alarm_app/src/view/home/room_list/room_list_view_model.dart';
 import 'package:alarm_app/util/helper/infinite_scroll_mixin.dart';
 import 'package:alarm_app/src/view/home/room_item.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-class RoomListView extends StatefulWidget {
+class RoomListView extends StatelessWidget {
   const RoomListView({super.key});
 
   @override
-  State<RoomListView> createState() => _RoomListViewState();
-}
-
-class _RoomListViewState extends State<RoomListView>
-    with InfiniteScrollMixin, WidgetsBindingObserver {
-  @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return BaseView(
+        viewModel: RoomListViewModel(),
+        builder: (context, viewModel) => Scaffold(
+              body: Center(
+                child: Text('홈'),
+              ),
+            ));
   }
 }
--- lib/src/view/home/room_list/room_list_view_model.dart
+++ lib/src/view/home/room_list/room_list_view_model.dart
@@ -1,8 +1,9 @@
 import 'dart:convert';
 
 import 'package:alarm_app/main.dart';
+import 'package:alarm_app/src/view/base_view_model.dart';
 import 'package:http/http.dart' as http;
 import 'package:alarm_app/src/model/room_model.dart';
 import 'package:flutter/cupertino.dart';
 
-class RoomListViewModel extends ChangeNotifier {}
+class RoomListViewModel extends BaseViewModel {}
