e5dc9346a960ad4282fc19e4bc16c72c649d2e0b
[APP1] 김성연 <jb5767sy123@gmail.com>
2024-08-13 18:04:20 +0900

    feature: (chat_view, chat_view_model): view  및 view model 업데이트

--- lib/src/repository/socket_repository.dart
+++ lib/src/repository/socket_repository.dart
@@ -1,5 +1,1 @@
-import 'package:alarm_app/main.dart';
-import 'package:alarm_app/src/model/msg_model.dart';
-import 'package:socket_io_client/socket_io_client.dart' as IO;
 
-class SocketRepository {}
--- lib/src/view/chat/chat_view_model.dart
+++ lib/src/view/chat/chat_view_model.dart
@@ -1,3 +1,15 @@
 import 'package:flutter/cupertino.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
 
-class ChatViewModel with ChangeNotifier {}
+class ChatViewModel with ChangeNotifier {
+  List<String> mymessage = [];
+
+  final TextEditingController controller = TextEditingController();
+  void sendMessage() {
+    if (controller.text.isNotEmpty) {
+      mymessage.add(controller.text);
+      controller.clear(); // 메시지 전송 후 입력창 비우기
+    }
+    notifyListeners();
+  }
+}
