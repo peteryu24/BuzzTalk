10bf8bb38c530fca47a061da63c4f883ac82f520
donghyeokkim <kdh301397@gmail.com>
2024-08-13 21:02:52 +0900

    [feature] local notification service 기능 작성 완료

--- lib/src/service/local_notification_service.dart
+++ lib/src/service/local_notification_service.dart
@@ -8,7 +8,7 @@ import 'package:timezone/data/latest_all.dart' as tz;
 ///왜 late로 하셨지? 페이로드가 뭘 의미하는거지?
 ///초기화가 복잡한 경우 late로 해야함 + 메모리 효율은 덤 비동기면 생성하는 시점에서 처리가 안되니 late키워드를 사용해서 받음.
 ///페이로드는 알람을 눌렀을 때 추가로 얻어올 데이터
-
+/// 타임존 데이터 초기화
 class LocalNotificationService {
   late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
 
@@ -60,29 +60,51 @@ class LocalNotificationService {
     tz.setLocalLocation(tz.getLocation('UTC'));
   }
 
-  Future<void> scheduleRoomNotification(Room room) async {
+  ///예약 알람 기능
+  Future<void> scheduleNotification({
+    required int id,
+    required String title,
+    required String body,
+    required DateTime scheduledDateTime,
+    String? payload,
+  }) async {
     final tz.TZDateTime scheduledDate =
-        tz.TZDateTime.from(room.startTime, tz.local);
+        tz.TZDateTime.from(scheduledDateTime, tz.local);
 
     const AndroidNotificationDetails androidPlatformChannelSpecifics =
-        AndroidNotificationDetails('room_channel_id', 'Room Notifications',
-            channelDescription: 'Notifications to join scheduled rooms',
+        AndroidNotificationDetails(
+            'default_channel_id', 'Default Notifications',
+            channelDescription: 'General notifications for the app',
             importance: Importance.max,
             priority: Priority.high,
             showWhen: true);
 
-    const NotificationDetails platformChannelSpecifics =
-        NotificationDetails(android: androidPlatformChannelSpecifics);
+    const DarwinNotificationDetails iosPlatformChannelSpecifics =
+        DarwinNotificationDetails(
+      presentAlert: true,
+      presentBadge: true,
+      presentSound: true,
+    );
+
+    const NotificationDetails platformChannelSpecifics = NotificationDetails(
+      android: androidPlatformChannelSpecifics,
+      iOS: iosPlatformChannelSpecifics,
+    );
 
     await flutterLocalNotificationsPlugin.zonedSchedule(
-        room.id, // Notification ID
-        'Time to Join ${room.name}',
-        'Your scheduled room is ready. Tap to join!',
+        id, // Notification ID
+        title,
+        body,
         scheduledDate,
         platformChannelSpecifics,
         uiLocalNotificationDateInterpretation:
             UILocalNotificationDateInterpretation.wallClockTime,
         matchDateTimeComponents: DateTimeComponents.time,
-        payload: room.id.toString()); // Room ID as payload
+        payload: payload);
+  }
+
+  ///예약 취소
+  Future<void> cancelAllNotifications() async {
+    await flutterLocalNotificationsPlugin.cancelAll();
   }
 }
--- lib/src/view/home/home_view.dart
+++ lib/src/view/home/home_view.dart
@@ -1,6 +1,8 @@
+import 'package:alarm_app/src/service/local_notification_service.dart';
 import 'package:alarm_app/src/view/home/my_room/my_room_view.dart';
 import 'package:alarm_app/src/view/home/room_list//room_list_view.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -12,7 +14,20 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      body: Center(
+        child: TextButton(
+          onPressed: () {
+            context.read<LocalNotificationService>().scheduleNotification(
+                  id: 0,
+                  title: 'title',
+                  body: 'body',
+                  scheduledDateTime: DateTime.now().add(Duration(seconds: 8)),
+                );
+          },
+          child: Text('das'),
+        ),
+      ),
+    );
   }
 }
