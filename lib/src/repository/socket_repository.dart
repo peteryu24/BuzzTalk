ce8c38c1b978033596668c4739f0d63a86bb0d23
[APP1] 김성연 <jb5767sy123@gmail.com>
2024-08-14 14:36:53 +0900

    bug: 충돌

--- lib/src/repository/socket_repository.dart
+++ lib/src/repository/socket_repository.dart
@@ -1,38 +1,3 @@
-import 'package:socket_io_client/socket_io_client.dart' as IO;
-
-class SocketRepository {
-  late IO.Socket _socket;
-
-  void initializeSocket(
-      {required String roomId, required Function(String) onMessageReceived}) {
-    _socket = IO.io(
-        'http://localhost:3001/chat',
-        IO.OptionBuilder()
-            .setTransports(['websocket'])
-            .disableAutoConnect()
-            .setQuery({'roomId': roomId})
-            .build());
-
-    _socket.onConnect((_) {
-      print('Connected to server');
-      _socket.emit('join', {'roomId': roomId, 'playerId': 'uniquePlayerId'});
-    });
-
-    _socket.onDisconnect((_) {
-      print('Disconnected from server');
-    });
-
-    _socket.on('msg', (data) {
-      onMessageReceived(data['msg']);
-    });
-
-    _socket.connect();
-  }
-
-  void sendMessage(String roomId, String message) {
-    _socket.emit('msg',
-        {'roomId': roomId, 'msg': message, 'playerId': 'uniquePlayerId'});
-  }
 import 'package:alarm_app/main.dart';
 import 'package:alarm_app/src/model/msg_model.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
