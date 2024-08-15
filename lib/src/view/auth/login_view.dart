878af9d0ae96d5ffd9f22ca47827c96f820d0730
peteryu24 <peteryu24@naver.com>
2024-08-15 18:54:04 +0900

    pubspec.yml provider version setting

--- lib/src/view/auth/login_view.dart
+++ lib/src/view/auth/login_view.dart
@@ -1,4 +1,5 @@
 // TODO: 임시
+// TODO: Remember me : SharedPreferences?
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:alarm_app/src/view/auth/login_view_model.dart';
