6307ab9c6d0b449d924acfc2c77a33e46f7522af
peteryu24 <peteryu24@naver.com>
2024-08-19 11:18:36 +0900

    roomModel changed

--- lib/src/model/room_model.dart
+++ lib/src/model/room_model.dart
@@ -1,47 +1,49 @@
-// TODO: 제거 변경
 class RoomModel {
-  //final int roomId;
-  final String roomName;
-  final DateTime startTime;
-  final DateTime endTime;
-  final int topicId;
-  final String playerId;
-  final DateTime createdAt;
-  final DateTime updatedAt;
+  final int? roomId; // roomId는 선택적
+  final String roomName; // roomName은 필수
+  final DateTime? startTime; // startTime은 선택적
+  final DateTime? endTime; // endTime은 선택적
+  final int topicId; // topicId는 필수
+  final String playerId; // playerId는 필수
+  final DateTime createdAt; // createdAt은 필수
+  final DateTime? updatedAt; // updatedAt은 선택적
 
-  RoomModel(
-      { //required this.roomId,
-      required this.roomName,
-      required this.startTime,
-      required this.endTime,
-      required this.topicId,
-      required this.playerId,
-      required this.createdAt,
-      required this.updatedAt});
+  RoomModel({
+    this.roomId,
+    required this.roomName,
+    this.startTime,
+    this.endTime,
+    required this.topicId,
+    required this.playerId,
+    required this.createdAt,
+    this.updatedAt,
+  });
 
   factory RoomModel.fromJson(Map<String, dynamic> json) {
     return RoomModel(
-      //roomId: json['roomId'],
+      roomId: json['roomId'],
       roomName: json['roomName'],
-      startTime: json['startTime'],
-      endTime: json['endTime'],
+      startTime:
+          json['startTime'] != null ? DateTime.parse(json['startTime']) : null,
+      endTime: json['endTime'] != null ? DateTime.parse(json['endTime']) : null,
       topicId: json['topicId'],
       playerId: json['playerId'],
-      createdAt: json['createdAt'],
-      updatedAt: json['updatedAt'],
+      createdAt: DateTime.parse(json['createdAt']),
+      updatedAt:
+          json['updatedAt'] != null ? DateTime.parse(json['updatedAt']) : null,
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
-      //'roomId': roomId,
+      'roomId': roomId,
       'roomName': roomName,
-      'startTime': startTime,
-      'endTime': startTime,
+      'startTime': startTime?.toIso8601String(),
+      'endTime': endTime?.toIso8601String(),
       'topicId': topicId,
       'playerId': playerId,
-      'createdAt': createdAt,
-      'updatedAt': updatedAt,
+      'createdAt': createdAt.toIso8601String(),
+      'updatedAt': updatedAt?.toIso8601String(),
     };
   }
 }
