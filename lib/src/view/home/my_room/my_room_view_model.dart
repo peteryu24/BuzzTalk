4b4e1aa4a99a8d0aa42af392a08a8642752313f0
Backer <backer@skuniv.ac.kr>
2024-08-21 12:21:19 +0900

    로그인 기능 정상 동작 확인

--- lib/src/view/home/my_room/my_room_view_model.dart
+++ lib/src/view/home/my_room/my_room_view_model.dart
@@ -1,11 +1,50 @@
-import 'dart:convert';
-
-import 'package:alarm_app/main.dart';
-import 'package:alarm_app/src/model/room_model.dart';
-import 'package:alarm_app/src/service/local_notification_service.dart';
+import 'package:alarm_app/src/repository/auth_repository.dart';
 import 'package:alarm_app/src/view/base_view_model.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'package:alarm_app/src/view/auth/chg_pwd_view.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+import 'package:alarm_app/src/view/auth/chg_pwd_view_model.dart';
 
-class MyRoomViewModel extends BaseViewModel {}
+class MyRoomViewModel extends BaseViewModel {
+  final AuthRepository authRepository;
+
+  MyRoomViewModel({required this.authRepository});
+
+  Future<void> logout(BuildContext context) async {
+    try {
+      await authRepository.logout();
+      GoRouter.of(context).go('/login');
+    } catch (e) {
+      // 오류 처리
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Failed to logout: $e")),
+      );
+    }
+  }
+
+  Future<void> deleteAccount(BuildContext context) async {
+    try {
+      await authRepository.deletePlayer();
+      GoRouter.of(context).go('/login');
+    } catch (e) {
+      // 오류 처리
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Failed to delete account: $e")),
+      );
+    }
+  }
+
+  void changePassword(BuildContext context) {
+    // 비밀번호 변경 화면으로 이동
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => ChangeNotifierProvider(
+          create: (_) =>
+              ChangePasswordViewModel(context.read<AuthRepository>()),
+          child: const ChangePasswordView(),
+        ),
+      ),
+    );
+  }
+}
